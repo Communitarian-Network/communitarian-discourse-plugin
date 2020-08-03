850c98b890a1210096e7e25f2cfbf5172f60b78d
ArturShaidullin <arthur.shaydullin@flatstack.com>
2020-08-03 20:16:39 +0300

    [#173960515] Add ability to create resolution (#1)

---
+++ app/controllers/communitarian/resolutions_controller.rb
@@ -0,0 +1,37 @@
+module Communitarian
+  class ResolutionsController < ::ApplicationController
+    requires_plugin Communitarian
+
+    before_action :ensure_logged_in
+
+    def create
+      manager = NewPostManager.new(current_user, permitted_params)
+
+      result = manager.perform
+      json = serialize_data(result, NewPostResultSerializer, root: false)
+      render json: json, status: result.success? ? :created : :unprocessable_entity
+    end
+
+    private
+
+    def permitted_params
+      default_params.merge(resolution_params).with_indifferent_access
+    end
+
+    def resolution_params
+      params.permit(:raw, :title, :category, :composer_open_duration_msecs, :typing_duration_msecs)
+    end
+
+    def default_params
+      {
+        archetype: "regular",
+        draft_key: "new_topic",
+        is_resolution: true,
+        is_warning: false,
+        nested_post: true,
+        shared_draft: false,
+        unlist_topic: false
+      }
+    end
+  end
+end
--- plugin.rb
+++ plugin.rb
@@ -2,10 +2,27 @@
 
 # name: communitarian
 # about: Common plugin with main features
-# version: 0.1
+# version: 0.2
 # authors: Flatstack
 # url: https://github.com/fs/communitarian-discourse-plugin
 
 enabled_site_setting :communitarian_enabled
 
-PLUGIN_NAME ||= 'Communitarian'
+register_asset "stylesheets/common/resolution-form.scss"
+
+PLUGIN_NAME ||= "communitarian"
+
+load File.expand_path("lib/communitarian/engine.rb", __dir__)
+
+after_initialize do
+  Topic.register_custom_field_type("is_resolution", :boolean)
+
+  # using Discourse "Topic Created" event to trigger a save.
+  # `opts[]` is how you pass the data back from the frontend into Rails
+  on(:topic_created) do |topic, opts, user|
+    if opts[:is_resolution] != nil
+      topic.custom_fields["is_resolution"] = opts[:is_resolution]
+      topic.save_custom_fields(true)
+    end
+  end
+end
