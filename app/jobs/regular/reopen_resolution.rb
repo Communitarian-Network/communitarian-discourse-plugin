966eb2c9cf70ff1c967573ddf36113cd8f813423
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-04 17:23:41 +0300

    ensure post exists

--- app/jobs/regular/reopen_resolution.rb
+++ app/jobs/regular/reopen_resolution.rb
@@ -5,8 +5,8 @@ module Jobs
     def execute(args)
       raise Discourse::InvalidParameters.new(:post_id) if args[:post_id].blank?
 
-      post = Post.find!(args[:post_id])
-      Communitarian::Resolution.reopen_weekly_resolution!(post)
+      post = Post.find(args[:post_id])
+      Communitarian::Resolution.reopen_weekly_resolution!(post) if post
     end
   end
 end
