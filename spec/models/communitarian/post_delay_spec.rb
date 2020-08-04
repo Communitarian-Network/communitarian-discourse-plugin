9f8e7899e3272244db014331065ba20dcb64acab
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-04 15:22:37 +0300

    [#174066511] Throttle posts (#2)
    * migrate to plugin
    
    * use locale
    
    * add specs for delayed posts
    
    * Address codestyle offences
    
    * Update post_delay_spec.rb
    
    * Update config/locales/server.en.yml
    
    Co-authored-by: ArturShaidullin <arthur.shaydullin@flatstack.com>
    
    * Update app/models/communitarian/user.rb
    
    Co-authored-by: ArturShaidullin <arthur.shaydullin@flatstack.com>
    
    * use term "dialog" instead of topic
    
    * Merge github.com:fs/communitarian-discourse-plugin into delay-posts-174066511
    
    * PLUGIN_NAME ||= "communitarian"
    
    * move user to top level package in model
    
    * move user to top level package in model
    
    Co-authored-by: ArturShaidullin <arthur.shaydullin@flatstack.com>

---
+++ spec/models/communitarian/post_delay_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Communitarian::PostDelay, type: :model do
+  describe ".call" do
+    subject(:result) { Communitarian::PostDelay.call(manager) }
+
+    let(:manager) { NewPostManager.new(user, topic_id: 1) }
+    let(:user) { mock("user") }
+
+    context "when user posted recently" do
+      before { user.stubs(:posted_recently?).returns(true) }
+
+      it "creates an error" do
+        expect(result.errors).to have_attributes(
+          full_messages: include("You can't post more than once in 5 minutes to this dialog")
+        )
+      end
+    end
+
+    context "when user has no recent posts" do
+      before { user.stubs(:posted_recently?).returns(false) }
+
+      it "does nothing" do
+        expect(result).to be_nil
+      end
+    end
+  end
+end
