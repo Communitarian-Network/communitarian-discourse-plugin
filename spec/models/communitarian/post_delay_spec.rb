f21059769cb5de0ed68a280cd0475ebc4665175f
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-05 16:54:11 +0300

    update specs

--- spec/models/communitarian/post_delay_spec.rb
+++ spec/models/communitarian/post_delay_spec.rb
@@ -4,13 +4,13 @@ require "rails_helper"
 
 RSpec.describe Communitarian::PostDelay, type: :model do
   describe ".call" do
-    subject(:result) { Communitarian::PostDelay.call(manager) }
+    subject(:result) { Communitarian::PostDelay.new(5.minutes).call(manager) }
 
     let(:manager) { NewPostManager.new(user, topic_id: 1) }
     let(:user) { mock("user") }
 
     context "when user posted recently" do
-      before { user.stubs(:posted_recently?).returns(true) }
+      before { user.stubs(:posted_after?).returns(true) }
 
       it "creates an error" do
         expect(result.errors).to have_attributes(
@@ -20,7 +20,7 @@ RSpec.describe Communitarian::PostDelay, type: :model do
     end
 
     context "when user has no recent posts" do
-      before { user.stubs(:posted_recently?).returns(false) }
+      before { user.stubs(:posted_after?).returns(false) }
 
       it "does nothing" do
         expect(result).to be_nil
