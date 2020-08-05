e8d21c4bd76b5388a06cc22d6af57bc98da89e7b
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-05 14:17:22 +0300

    Address codestyle offences

--- lib/communitarian/engine.rb
+++ lib/communitarian/engine.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Communitarian
   class Engine < ::Rails::Engine
     engine_name "Communitarian".freeze
--- lib/communitarian_constraint.rb
+++ lib/communitarian_constraint.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommunitarianConstraint
   def matches?(request)
     SiteSetting.communitarian_enabled
