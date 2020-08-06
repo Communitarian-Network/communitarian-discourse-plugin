69a8dcd5560243db73036563c64bf02b87d377f5
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-06 18:15:47 +0300

    [#174188741] Customize posting delay (#6)
    * add setting for delay
    
    * enable setting
    
    * use delay value form settings
    
    * update specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 module Communitarian
   refine User do
-    def posted_recently?(topic_id)
-      self.posts.where(topic_id: topic_id).where("created_at > ?", 5.minutes.ago).exists?
+    def posted_after?(timestamp, topic_id)
+      self.posts.where(topic_id: topic_id).where("created_at > ?", timestamp).exists?
     end
   end
 end
