e28cbab26fca10d4e690cdb71ecbfb7bd6db25c6
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-06 12:46:54 +0300

    Address @Vadim-Kurnatovskiy review

--- app/models/communitarian/resolution.rb
+++ app/models/communitarian/resolution.rb
@@ -7,15 +7,14 @@ module Communitarian
   class Resolution
     using Communitarian
 
-    def self.reopened_poll_post_attributes
-      %w[user_id topic_id raw post_type last_editor_id last_version_at user_deleted public_version]
-    end
+    REOPENED_RESOLUTION_ATTRIBUTES = %w[
+      user_id topic_id raw post_type last_editor_id last_version_at user_deleted public_version
+    ].freeze
 
     def self.reopen_weekly_resolution!(original_post)
-      original_poll = original_post.polls.first
       return if original_post.user_deleted || !original_post.resolution?
 
-      post_attributes = original_post.attributes.slice(*self.reopened_poll_post_attributes)
+      post_attributes = original_post.attributes.slice(*self.REOPENED_RESOLUTION_ATTRIBUTES)
       post = Post.create!(post_attributes)
       poll = post.reload.polls.first
       poll.update!(close_at: self.next_close_time, is_weekly: true)
@@ -23,12 +22,12 @@ module Communitarian
     end
 
     def self.schedule_jobs(post)
-      ::DiscoursePoll::Poll.schedule_jobs(post)
       return unless post.resolution?
 
+      ::DiscoursePoll::Poll.schedule_jobs(post)
       job_args = { post_id: post.id }
       Jobs.cancel_scheduled_job(:reopen_resolution, job_args)
-      Jobs.enqueue_at(self.next_reopen_time(post.polls.first.close_at), :reopen_resolution , job_args)
+      Jobs.enqueue_at(self.next_reopen_time(post.polls.first.close_at), :reopen_resolution, job_args)
     end
 
     def self.next_close_time(today = self.current_time)
