f839802e75527bfe352ad69200ab117598f73c19
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2020-08-14 13:41:56 +0300

    Removed unused action

--- app/controllers/communitarian/page_controller.rb
+++ app/controllers/communitarian/page_controller.rb
@@ -3,9 +3,5 @@
 module Communitarian
   class PageController < ::ApplicationController
     requires_plugin Communitarian
-
-    def index
-      render json: { title: "Hello world!" }
-    end
   end
 end
