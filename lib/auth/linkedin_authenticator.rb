e39425c0c5f6a6840286ffadf344e1dd6e50b25a
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2020-08-17 16:34:35 +0300

    apply request changes

--- lib/auth/linkedin_authenticator.rb
+++ lib/auth/linkedin_authenticator.rb
@@ -35,17 +35,12 @@ class Auth::LinkedinAuthenticator < ::Auth::OAuth2Authenticator
       provider: oauth2_provider
     }
 
-    username_max_length = SiteSetting.max_username_length.to_i
-    username_id = result.user ? result.user.id : Time.current.to_i
-    result.username = username = [data[:first_name], data[:last_name], username_id].join(".")[0, username_max_length]
+    result.username = username = generate_username(data, result.user)
 
-    if result.user && email && (result.user.email != email)
-      begin
-        result.user.primary_email.update!(email: email)
-        result.user.username.update!(username: username)
-      rescue
-        used_by = User.find_by_email(email)&.username
-        Rails.logger.warn("FAILED to update email for #{user.username} to #{email} cause it is in use by #{used_by}")
+    if result.user
+      ActiveRecord::Base.transaction do
+        update_email(result, email)
+        update_username(result, username)
       end
     end
 
@@ -63,5 +58,36 @@ class Auth::LinkedinAuthenticator < ::Auth::OAuth2Authenticator
 
   def enabled?
     SiteSetting.linkedin_enabled
+  end
+
+  private
+
+  def update_email(result, email)
+    if email && (result.user.email != email)
+      begin
+          result.user.primary_email.update!(email: email)
+      rescue
+        used_by = User.find_by_email(email)&.username
+        Rails.logger.warn("FAILED to update email for #{user.username} to #{email} cause it is in use by #{used_by}")
+      end
+    end
+  end
+
+  def update_username(result, username)
+    if username && (result.user.username != username)
+      begin
+          result.user.username.update!(username: username)
+      rescue
+        used_by = User.find_by_username(username)&.username
+        Rails.logger.warn("FAILED to update username for #{user.username} to #{username} cause it is in use by #{used_by}")
+      end
+    end
+  end
+
+  def generate_username(data, user)
+    username_max_length = SiteSetting.max_username_length.to_i
+    username_id = user&.id || Time.current.to_i
+
+    [data[:first_name], data[:last_name], username_id].join(".")[0, username_max_length]
   end
 end
