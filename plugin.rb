2cca50603bcc0edadeea8fb1dc3e15e62474c07c
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2020-09-02 23:17:40 +0300

    Choose verification modal stylize

--- plugin.rb
+++ plugin.rb
@@ -20,6 +20,7 @@ enabled_site_setting :communitarian_enabled
   "stylesheets/common/landing.scss",
   "stylesheets/common/communities-page.scss",
   "stylesheets/common/create-account-modal.scss",
+  "stylesheets/common/choose-verification-way-modal.scss",
   "stylesheets/common/community-page.scss",
   "stylesheets/common/dialog-list.scss",
   "stylesheets/common/dialog-list-item.scss",
