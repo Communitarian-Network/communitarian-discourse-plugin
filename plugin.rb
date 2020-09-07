57e343f9f46b0e35d04c2945aca1b7b929a8d510
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2020-09-08 01:33:49 +0300

    Forgot password modal

--- plugin.rb
+++ plugin.rb
@@ -21,6 +21,7 @@ enabled_site_setting :communitarian_enabled
   "stylesheets/common/communities-page.scss",
   "stylesheets/common/create-account-modal.scss",
   "stylesheets/common/login-modal.scss",
+  "stylesheets/common/forgot-password-modal.scss",
   "stylesheets/common/choose-verification-way-modal.scss",
   "stylesheets/common/verification-intents-modal.scss",
   "stylesheets/common/payment-details-modal.scss",
