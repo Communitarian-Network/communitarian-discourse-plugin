c386221b5b77facd26d27aa097a9073a6c2cdb53
IlgizGilmanov <46020998+IlgizGilmanov@users.noreply.github.com>
2020-09-09 20:13:51 +0300

    173941827 Login flow stylize (#44)

--- plugin.rb
+++ plugin.rb
@@ -21,6 +21,8 @@ enabled_site_setting :communitarian_enabled
   "stylesheets/common/communities-page.scss",
   "stylesheets/common/dialog-page.scss",
   "stylesheets/common/create-account-modal.scss",
+  "stylesheets/common/login-modal.scss",
+  "stylesheets/common/forgot-password-modal.scss",
   "stylesheets/common/choose-verification-way-modal.scss",
   "stylesheets/common/payment-details-modal.scss",
   "stylesheets/common/verification-intents.scss",
