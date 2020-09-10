e622240222e4b51df9ad7a85a90ccf1c224cde9e
ArturShaidullin <arthur.shaydullin@flatstack.com>
2020-09-10 04:38:28 +0300

    add billing address to credit card sign up

---
+++ db/migrate/20200904155200_create_default_user_fields.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class CreateDefaultUserFields < ActiveRecord::Migration[6.0]
+  def up
+    UserField.create!(
+      id: 123_001,
+      name: "Billing Address",
+      field_type: "text",
+      editable: false,
+      description: "Billing Address",
+      required: false,
+      show_on_profile: true,
+      show_on_user_card: true,
+      position: 1
+    )
+  end
+
+  def down
+    UserField.where(id: [123_001]).destroy_all
+  end
+end
