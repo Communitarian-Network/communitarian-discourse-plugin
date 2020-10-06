d072a7a078f543725fd6b410fb10b1c3f950c5c0
Vadim Kurnatovskiy <49876756+Vadim-Kurnatovskiy@users.noreply.github.com>
2020-10-06 17:07:44 +0300

    [#175074401] community and resolution code should not be editable (#101)
    * community and resolution code should not be editable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,18 +8,19 @@ RSpec.describe User, type: :model do
   using Communitarian
 
   let(:user) { Fabricate(:user) }
+  let(:topic) { Fabricate(:topic) }
 
   describe ".posted_after?" do
     subject { user.posted_after?(5.minutes.ago, post.topic_id) }
 
     context "when there is a fresh post in the topic by the user" do
-      let(:post) { Fabricate(:post, user: user, created_at: 3.minutes.ago) }
+      let(:post) { Fabricate(:post, user: user, created_at: 3.minutes.ago, topic: topic) }
 
       it { is_expected.to eq true }
     end
 
     context "when there is an old post in the topic by the user" do
-      let(:post) { Fabricate(:post, user: user, created_at: 7.minutes.ago) }
+      let(:post) { Fabricate(:post, user: user, created_at: 7.minutes.ago, topic: topic) }
 
       it { is_expected.to eq false }
     end
