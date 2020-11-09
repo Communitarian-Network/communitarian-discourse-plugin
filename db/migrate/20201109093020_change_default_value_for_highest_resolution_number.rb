a91b962ced83aa2299b62710e75a6e49b290b196
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2020-11-09 12:48:12 +0300

    frozen string

--- db/migrate/20201109093020_change_default_value_for_highest_resolution_number.rb
+++ db/migrate/20201109093020_change_default_value_for_highest_resolution_number.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeDefaultValueForHighestResolutionNumber < ActiveRecord::Migration[6.0]
   def change
     change_column_default :categories, :highest_resolution_number, 0
